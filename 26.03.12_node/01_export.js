5788effe5c3fb7f9ec6b7843f71f05db690a0f17
gee031022-art <gee031022@gmail.com>
2026-03-12 17:08:53 +0900

    Add files via upload

---
+++ 26.03.12_node/01_export.js
@@ -0,0 +1,6 @@
+let base = 100;
+const a = (x) => base + x;
+const t = () => {
+  console.log('module내부에서 this는', this);
+};
+export { a as add, t as test };
---
+++ 26.03.12_node/01_import.js
@@ -0,0 +1,3 @@
+import { add } from './01_import.html';
+console.log = add(4);
+test();
